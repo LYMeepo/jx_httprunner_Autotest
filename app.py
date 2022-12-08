106503e0aeb8258528536554c3d13f311a50ac15
jinxiao <jinxiao@zhiyuanhui.com>
2022-12-08 17:20:30 +0800

    增加用例管理

--- app.py
+++ app.py
@@ -1,53 +1,91 @@
 import time
 
 from flask import Flask, render_template, request
-import subprocess, os, shutil
+import subprocess, os, shutil, zipfile
 from concurrent.futures import ThreadPoolExecutor
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
-# apps.config['UPLOAD_FOLDER'] = 'files'
+
+template_folder = os.getcwd() + '/templates'
+static_folder = os.getcwd() + '/static'
+static_url_path = '/static'
+
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder,static_url_path=static_url_path)
+app.config['UPLOAD_FOLDER'] = 'cases_bak'
+
 log = ""
 reports_dir = os.sep.join(['templates', 'reports'])
 executor = ThreadPoolExecutor(1)
 path = ""
+api_path = "api"
 
 
-@app.route(path + '/')
+@app.route('/')
 def index():
+    global api_path
+    api_path = "api"
     templateData = {"path": path}
     return render_template(r'index.html', **templateData)
 
 
-@app.route(path + '/hruntest', methods=["GET"])
+@app.route('/hruntest', methods=["GET"])
 def hruntest():
-    templateData = {"envs": get_envs(),
-                    "path": path
-                    }
+    global api_path
+    api_path = "api"
+    templateData = {"path": path,
+                    "envs": get_envs(),
+                    "cases": get_cases()}
 
     return render_template(r'hruntest.html', **templateData)
 
 
-@app.route(path + '/hruntest_success', methods=["POST"])
+@app.route('/hruntest_success', methods=["POST"])
 def hruntest_success():
     templateData = {"path": path,
                     "clew": "执行成功"}
+
+    sel_cases = request.form.get('sel_cases')
     sel_env = request.form.get('sel_env')
+
+    switch_cases(sel_cases)
     switch_env(sel_env)
+
     time.sleep(1)
     hrun_start(sel_env)
     return render_template(r'success.html', **templateData)
 
 
-@app.route(path + '/log')
+@app.route('/log')
 def log():
+    global api_path
+    api_path = "api"
     templateData = {'log': log,
                     "path": path
                     }
     return render_template(r'log.html', **templateData)
 
+# 切换测试用例集合
+def switch_cases(sel_cases):
+    #移除api、testcases、testsuites
+    shutil.rmtree("api")
+    shutil.rmtree("testcases")
+    shutil.rmtree("testsuites")
 
-@app.route(path + '/reports', methods=["GET", "POST"])
+    #从cases_bak复制api、testcases、testsuites、debugtalk.py
+    api_dir = os.sep.join(['cases_bak',sel_cases,'api'])
+    testcases_dir = os.sep.join(['cases_bak', sel_cases, 'testcases'])
+    testsuites_dir = os.sep.join(['cases_bak', sel_cases, 'testsuites'])
+    debugtalk_dir = os.sep.join(['cases_bak', sel_cases, 'debugtalk.py'])
+    shutil.copytree(api_dir, r"api")
+    shutil.copytree(testcases_dir, r"testcases")
+    shutil.copytree(testsuites_dir, r"testsuites")
+    shutil.copyfile(debugtalk_dir, 'debugtalk.py')
+
+
+#========================测试报告============================
+@app.route('/reports', methods=["GET", "POST"])
 def reports():
+    global api_path
+    api_path = "api"
     reports = []
     for file_name in os.listdir(reports_dir):
         reports.append(file_name)
@@ -58,26 +96,29 @@ def reports():
     return render_template(r'reports.html', **templateData)
 
 
-@app.route(path + '/report', methods=["GET", "POST"])
+@app.route('/report', methods=["GET", "POST"])
 def report():
     btn = request.form.get('btn')
     templateData = {"path": path}
     return render_template(r'reports/' + btn, **templateData)
+#========================测试报告============================
 
-
-@app.route(path + '/env_manager', methods=["GET"])
+#========================配置管理============================
+@app.route('/env_manager', methods=["GET"])
 def env_manager():
+    global api_path
+    api_path = "api"
     templateData = {"path": path}
     return render_template(r'env_manager.html', **templateData)
 
 
-@app.route(path + '/addenv', methods=["GET"])
+@app.route('/addenv', methods=["GET"])
 def addenv():
     templateData = {"path": path}
     return render_template(r'addenv.html', **templateData)
 
 
-@app.route(path + '/addenv_success', methods=["POST"])
+@app.route('/addenv_success', methods=["POST"])
 def addenv_success():
     templateData = {"path": path,
                     "clew": "添加成功"}
@@ -88,7 +129,7 @@ def addenv_success():
     return render_template(r'success.html', **templateData)
 
 
-@app.route(path + '/delete_env', methods=["GET"])
+@app.route('/delete_env', methods=["GET"])
 def delete_env():
     templateData = {"envs": get_envs(),
                     "path": path
@@ -96,7 +137,7 @@ def delete_env():
     return render_template(r'delete_env.html', **templateData)
 
 
-@app.route(path + '/delete_env_success', methods=["POST"])
+@app.route('/delete_env_success', methods=["POST"])
 def delete_env_success():
     templateData = {"path": path,
                     "clew": "删除成功"}
@@ -105,7 +146,7 @@ def delete_env_success():
     return render_template(r'success.html', **templateData)
 
 
-@app.route(path + '/env_detail_sel', methods=["GET"])
+@app.route('/env_detail_sel', methods=["GET"])
 def env_detail_sel():
     templateData = {"envs": get_envs(),
                     "path": path
@@ -113,7 +154,7 @@ def env_detail_sel():
     return render_template(r'env_detail_sel.html', **templateData)
 
 
-@app.route(path + '/env_detail', methods=["POST"])
+@app.route('/env_detail', methods=["POST"])
 def env_detail():
     sel_env = request.form.get('sel_env')
     env_detail_str = get_env_detail(sel_env)
@@ -130,12 +171,65 @@ def add_env_file(env_content, env_name):
     with open(env, 'w', newline='') as f:
         f.write(env_content)
 
+# 删除文件夹
+def delete_dir(dir_name):
+    env_file_src = os.sep.join(['cases_bak', dir_name])
+    if os.path.exists(env_file_src):
+        shutil.rmtree(env_file_src)
 
+# 删除文件
 def delete_env_file(env_name):
     env_file_src = os.sep.join(['env', env_name])
     if os.path.exists(env_file_src):
         os.remove(env_file_src)
+#========================配置管理============================
 
+#========================用例管理============================
+@app.route('/case_manager', methods=["GET"])
+def case_manager():
+    global api_path
+    api_path = "api"
+    templateData = {"path": path}
+    return render_template(r'cases/case_manager.html', **templateData)
+
+@app.route('/api_add', methods=["GET"])
+def api_add():
+    templateData = {"path": path}
+
+    return render_template(r'cases/api_add.html', **templateData)
+
+@app.route('/api_add_success', methods=["POST"])
+def api_add_success():
+    templateData = {"path": path,
+                    "clew": "添加成功"}
+    # 保存压缩文件到cases_bak
+    file = request.files['file']
+    save_path = os.path.join(app.config['UPLOAD_FOLDER'])
+    save_path = os.sep.join([save_path, r"cases.zip"])
+    file.save(save_path)
+    # 解压压缩文件
+    api_dir = os.sep.join(["cases_bak"])
+    zf = zipfile.ZipFile(save_path)
+    zf.extractall(path=api_dir)
+
+    return render_template(r'success.html', **templateData)
+
+
+@app.route('/api_del', methods=["GET"])
+def api_del():
+    templateData = {"path": path,
+                    "cases": get_cases()}
+    return render_template(r'cases/api_del.html', **templateData)
+
+@app.route('/api_del_success', methods=["POST"])
+def api_del_success():
+    templateData = {"path": path,
+                    "clew": "删除成功"}
+    sel_cases = request.form.get('sel_cases')
+    delete_dir(sel_cases)
+    return render_template(r'success.html', **templateData)
+
+#========================用例管理============================
 
 # 初始化日志文件
 def init_log_file():
@@ -164,6 +258,10 @@ def get_env_detail(env_name):
         env_detail_str = f.read()
     return env_detail_str
 
+# 获取api
+def get_cases():
+    for _, api_dirs, _ in os.walk("cases_bak"):
+        return api_dirs
 
 # 加载日志
 def load_log():
@@ -188,7 +286,7 @@ def hrun_start(env_name):
 def get_now_time():
     return time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
 
-
 if __name__ == '__main__':
+
     init_log_file()
-    app.run(host="0.0.0.0", port=13243)
+    app.run(host="0.0.0.0", port=80)
--- debugtalk.py
+++ debugtalk.py
@@ -67,6 +67,73 @@ def connect_ck():
 
     return client
 
+#查询设备数量
+def sensor(type):
+    ck = connect_ck()
+    sql = r"SELECT count( 1 ) AS sensorSum, count( CASE WHEN ( expired_time < '2000-01-01' ) THEN 1 ELSE NULL END ) AS inactivatedNum, count( CASE WHEN (( expired_time > '2000-01-01' ) AND ( expired_time < now())) THEN 1 ELSE NULL END ) AS offline, count( CASE WHEN ( expired_time > now()) THEN 1 ELSE NULL END ) AS ONLINE FROM sensor_all_info final WHERE deleted =0"
+    res = ck.execute(sql)
+    ck.disconnect()
+    # 设备总量
+    if type == 0:
+        return res[0][0]
+    # 未激活
+    if type == 1:
+        return res[0][1]
+    # 离线
+    if type == 2:
+        return res[0][2]
+    # 在线
+    if type == 3:
+        return res[0][3]
+    return
+
+
+#接入日志总数
+def sensor_log_total():
+    ck = connect_ck()
+    sql = r"select sum(window_count) from sensor_sum final"
+    res = ck.execute(sql)
+    ck.disconnect()
+    return round(res[0][0]/10000,4)
+
+
+
+#今日接入日志数
+def sensor_log_total_today():
+    ck = connect_ck()
+    sql = r"select sum(window_count) from sensor_sum final where p_date = today()"
+    res = ck.execute(sql)
+    ck.disconnect()
+    return res[0][0]
+
+#接入设备总数
+def sensor_info_total():
+    ck = connect_ck()
+    sql = r"select count(1) from sensor_all_info final where deleted = 0;"
+    res = ck.execute(sql)
+    ck.disconnect()
+    return res[0][0]
+
+#今日接入设备数
+def sensor_info_total_today():
+    ck = connect_ck()
+    sql = r"select count(*) from sensor_all_info where create_time >= '" + begin_date_today() + "' and create_time <= '" + end_date_today() + "'  and deleted  = 0"
+    res = ck.execute(sql)
+    ck.disconnect()
+    return res[0][0]
+
+#今日接入设备种类
+def sensor_type_total_today():
+    ck = connect_ck()
+    sql = r"select type_code from sensor_all_info where create_time >= '" + begin_date_today() + "' and create_time <= '" + end_date_today() + "'  and deleted  = 0 group by type_code"
+
+    res = ck.execute(sql)
+    ck.disconnect()
+    return len(res)
+
+
+
+
 #=========================================CK===================================================
 
 
@@ -80,6 +147,215 @@ def connect_mysql(db):
     conn = pymysql.connect(host=host, port=port, user=username, passwd=passwd, db=db)
 
     return conn
+
+#获取设备类型数量
+def get_sensorType_total():
+    conn = connect_mysql("iot-sensor")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM sensor_type_percentage")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#获取物模型数量
+def model_total():
+    conn = connect_mysql("iot-sensor")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM physical_model_info where deleted = 0")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+
+#今日新增事件1
+def eventTodayTotal():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM event_info_message where create_time > DATE_FORMAT(NOW(),'%Y-%m-%d 00:00:00') and create_time <DATE_FORMAT(NOW(),'%Y-%m-%d 23:59:59')")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#发现事件总数1
+def eventTotal():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM event_info_message")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#今日新增算法 1
+def ruleTodayTotal():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM event_info where deleted = 0 and event_status = 2 and create_time > DATE_FORMAT(NOW(),'%Y-%m-%d 00:00:00') and create_time <DATE_FORMAT(NOW(),'%Y-%m-%d 23:59:59')")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#运行算法总个数 1
+def ruleTotal():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM event_info where deleted = 0 and event_status = 2")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#一般事件
+def statisticsByLevel_1_3():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM event_info_message where deleted = 0 and event_level_name = 'I级' or event_level_name = 'II级' or event_level_name = 'III级'")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#告警事件
+def statisticsByLevel_4_5():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(
+        r"SELECT count(*) FROM event_info_message where deleted = 0 and event_level_name = 'IV级' or event_level_name = 'V级'")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+#重大事件
+def statisticsByLevel_warning():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(
+        r"SELECT count(*) FROM event_info_message where deleted = 0 and event_level_name = '告警'")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#已流转事件数
+def message_count():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM info_message")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+#已完成事件数
+def message_end():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM info_message where status = 2")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+#未完成事件数
+def message_start():
+    conn = connect_mysql("iot-scene")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM info_message where status = 1")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#数据流转次数
+def shareData_totalTimes():
+    conn = connect_mysql("iot-share")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM task_info WHERE deleted = 0")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    print(res_arr[0][0])
+    return res_arr[0][0]
+
+#数据流转总流量
+def shareData_totalNum():
+    conn = connect_mysql("iot-share")
+    cur = conn.cursor()
+    cur.execute(r"SELECT SUM(share_data_num) FROM task_info WHERE deleted = 0")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    print(float(res_arr[0][0]/10000))
+    return float(res_arr[0][0]/10000)
+
+#设备、日志数据共享   business_type: 1是设备 2是日志
+def share_total(business_type):
+    conn = connect_mysql("iot-share")
+    cur = conn.cursor()
+    cur.execute(r"select SUM(share_data_num) share_data_num from task_info  where deleted = 0 and business_type = " + str(business_type) +" GROUP BY business_type")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#今日设备、日志数据共享   business_type: 1是设备 2是日志
+def share_total_today(business_type):
+    conn = connect_mysql("iot-share")
+    cur = conn.cursor()
+    if business_type == 1:
+        cur.execute(r"select sum(num) from sensor_statistic where time = '" + get_now_time_day() + "' and deleted = 0")
+    if business_type == 2:
+        cur.execute(r"select IFNULL(sum(num),0) from sensor_log_statistic where time = '" + get_now_time_day() + "' and deleted = 0")
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
+
+#设备共享任务总数 business_type: 1是设备 2是日志
+def task_info_total(business_type):
+    conn = connect_mysql("iot-share")
+    cur = conn.cursor()
+    cur.execute(r"SELECT count(*) FROM task_info where deleted = 0 and business_type = " + str(business_type))
+    res_arr = []
+    for r in cur:
+        res_arr.append(r)
+    cur.close()
+    conn.close()
+    return res_arr[0][0]
 
 
 #=========================================MYSQL================================================
